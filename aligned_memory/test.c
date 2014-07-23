6c7b61fba3bc7546e7d0c60818822530a91ef248
o <o@immerda.ch>
2014-07-23 12:50:08 -0400

    keep pages alive and release later

--- aligned_memory/test.c
+++ aligned_memory/test.c
@@ -6,32 +6,51 @@ int main(){
   alignedMemoryManagerInit();
   int chunkAlignment = memoryChunkAlignment();
 
-  for (int i = chunkAlignment - 100;
-       i < 2*chunkAlignment;
-       i += 6987) {
-    MemoryChunkHeader * chunk = allocateAligned(i);
+  printf("chunksize is %dk\n", chunkAlignment/1024);
+
+  int start      = chunkAlignment - 100;
+  int step       = 6987;
+  int end        = 2*chunkAlignment;
+  int num_chunks = (end-start) / step;
+
+  MemoryChunkHeader * chunks[num_chunks];
+
+  printf("Allocating some pages\n");
+  for (int i = 0; i < num_chunks; i++) {
+    int allocate = start + (i * step);
+    chunks[i] = allocateAligned(allocate);
+
+    MemoryChunkHeader * chunk = chunks[i];
     if (chunk == NULL) {
       printf("could not get chunk of size %d\n", i);
       continue;
     }
 
-    char              * data  = (char*)chunkData(chunk);
+    char * data = (char*)chunkData(chunk);
 
-    float   overalloc = 100.0 * (((float) chunk->length /
-                                  (float) i) - 1.0);
+    float overalloc = 100.0 * (((float) chunk->length /
+                                (float) allocate) - 1.0);
+    // printf("%p : requested %d bytes got %d (%f%% overallocation)\n",
+    //        chunk, allocate, chunk->length, overalloc);
+
     assert(overalloc < 1.0);
     assert(isAligned(chunk, chunkAlignment));
 
-    printf("chunksize is %dk\n", chunkAlignment/1024);
-    printf("requested %d bytes got %d (%f%% overallocation)\n",
-           i, chunk->length, overalloc);
-    printf("%p : chunk header\n", chunk);
-
-    for (int i = 0; i < chunk->length; i++, data++) {
+    for (int j = 0; j < chunk->length; j++, data++) {
       assert(chunkFromPtr(data) == chunk || !isMaskable(data, chunk));
       assert((unsigned int)*data == 0);
-      *data = 6;
-      assert((unsigned int)*data == 6);
+      *data = i%128;
+      assert((unsigned int)*data == i%128);
+    }
+  }
+
+  printf("Freeing memory\n");
+  for (int i = num_chunks-1; i >= 0; i--) {
+    MemoryChunkHeader * chunk = chunks[i];
+    char *              data  = (char*)chunkData(chunk);
+
+    for (int j = 0; j < chunk->length; j++, data++) {
+      assert((unsigned int)*data == i%128);
     }
 
     freeChunk(chunk);
